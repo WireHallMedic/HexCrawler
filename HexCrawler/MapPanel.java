10bb9dff9312e7829a5f7a1760654593d23c64cd
Michael <MichaelWidler@yahoo.com>
2025-11-27 08:38:37 -0800

    Implement mouseListener.

--- HexCrawler/MapPanel.java
+++ HexCrawler/MapPanel.java
@@ -26,6 +26,7 @@ public class MapPanel extends JPanel implements HexCrawlerConstants, MouseListen
          if(i >= Terrain.values().length)
             i = 0;
       }
+      addMouseListener(this);
    }
    
    public void setTile(int x, int y, Terrain t)
@@ -39,39 +40,26 @@ public class MapPanel extends JPanel implements HexCrawlerConstants, MouseListen
       double mouseLocY = me.getY() / scale;
       int lastX = -1;
       int lastY = -1;
-      int lastDist = 1000000;
+      double lastDist = 1000000.0;
       for(int x = 0; x < hexArray.length; x++)
       for(int y = 0; y < hexArray[0].length; y++)
       {
-         
+         double newX = mouseLocX - (getXInset(x, y) + (HEX_WIDTH / 2));
+         double newY = mouseLocY - (getYInset(x, y) + (HEX_HEIGHT / 2));
+         double newDist = (newX * newX) + (newY * newY);
+         if(newDist < lastDist && newDist < (R_LONG * R_LONG))
+         {
+            lastX = x;
+            lastY = y;
+            lastDist = newDist;
+            System.out.println(x + ", " + y);
+         }
       }
    }
    public void mousePressed(MouseEvent me){}
    public void mouseReleased(MouseEvent me){}
    public void mouseEntered(MouseEvent me){}
    public void mouseExited(MouseEvent me){}
-   
-   private void paintHex(Graphics2D g2d, MapHex tile, int xPos, int yPos)
-   {
-      double[] xPoints = indentX(xPos, yPos, HEX_POINTS_X);
-      double[] yPoints = indentY(xPos, yPos, HEX_POINTS_Y);
-      xPoints = scale(scale, xPoints);
-      yPoints = scale(scale, yPoints);
-      // draw background
-      g2d.setColor(tile.getBackground());
-      g2d.fillPolygon(doubleToInt(xPoints), doubleToInt(yPoints), xPoints.length);
-      // draw border
-      g2d.setColor(Color.BLACK);
-      g2d.drawPolygon(doubleToInt(xPoints), doubleToInt(yPoints), xPoints.length);
-      // draw image
-      if(tile.getImage() != null)
-      {
-         BufferedImage img = tile.getScaledImage(scale);
-         double xOrigin = getXInset(xPos, yPos) + R_SHORT - (SQUARE_SIDE / 2);
-         double yOrigin = getYInset(xPos, yPos) + R_LONG - (SQUARE_SIDE / 2);
-         g2d.drawImage(img, (int)(xOrigin * scale), (int)(yOrigin * scale), null);
-      }
-   }
    
    private double[] indentX(int xPos, int yPos, double[] arr)
    {
@@ -133,5 +121,27 @@ public class MapPanel extends JPanel implements HexCrawlerConstants, MouseListen
       for(int x = 0; x < w; x++)
       for(int y = 0; y < h; y++)
          paintHex(g2d, hexArray[x][y], x, y);
+   }
+   
+   private void paintHex(Graphics2D g2d, MapHex tile, int xPos, int yPos)
+   {
+      double[] xPoints = indentX(xPos, yPos, HEX_POINTS_X);
+      double[] yPoints = indentY(xPos, yPos, HEX_POINTS_Y);
+      xPoints = scale(scale, xPoints);
+      yPoints = scale(scale, yPoints);
+      // draw background
+      g2d.setColor(tile.getBackground());
+      g2d.fillPolygon(doubleToInt(xPoints), doubleToInt(yPoints), xPoints.length);
+      // draw border
+      g2d.setColor(Color.BLACK);
+      g2d.drawPolygon(doubleToInt(xPoints), doubleToInt(yPoints), xPoints.length);
+      // draw image
+      if(tile.getImage() != null)
+      {
+         BufferedImage img = tile.getScaledImage(scale);
+         double xOrigin = getXInset(xPos, yPos) + R_SHORT - (SQUARE_SIDE / 2);
+         double yOrigin = getYInset(xPos, yPos) + R_LONG - (SQUARE_SIDE / 2);
+         g2d.drawImage(img, (int)(xOrigin * scale), (int)(yOrigin * scale), null);
+      }
    }
 }
